1c13d919cebec3c54ef69c683b70789f927a9e47
eangele1 <ezerangeles1@gmail.com>
2019-10-27 23:55:30 -0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import SQLite from 'react-native-sqlite-storage';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <View>
+      <Text style={{ textAlign: "center" }}>This has nothing in it. For now...</Text>
+    </View>
+  );
+};
+
+export default App;
