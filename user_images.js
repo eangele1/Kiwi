38e7b0f05a24bb2f791cc0a77f1161b79512cea2
eangele1 <ezerangeles1@gmail.com>
2019-12-12 11:26:13 -0800

    Added images location file

---
+++ user_images.js
@@ -0,0 +1,10 @@
+const images = {
+	"100123456": {
+		uri: require('./assets/images/100123456.jpg')
+	},
+	"100789123": {
+		uri: require('./assets/images/100789123.jpg')
+	}
+}
+
+export { images };
