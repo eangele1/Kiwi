f5b9956cc477e9da41e771f8f9e1b9acefb06fe6
eangele1 <ezerangeles1@gmail.com>
2019-12-11 20:50:09 -0800

    Archive Screen implemented

--- Archive.js
+++ Archive.js
@@ -1,13 +1,313 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
+import { openDatabase } from 'react-native-sqlite-storage';
+import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
+import { Button } from 'native-base';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
+let user_ID;
+let db;
 
 export default class ArchiveScreen extends React.PureComponent {
+
+    militaryToNormalTime = (string) => {
+        var date = new Date("February 04, 2011 " + string);
+        var hours = date.getHours();
+        var am = true;
+        if (hours > 12) {
+            am = false;
+            hours -= 12;
+        } else if (hours == 12) {
+            am = false;
+        } else if (hours == 0) {
+            hours = 12;
+        }
+
+        var minutes = date.getMinutes();
+
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+
+        var timeString = hours + ":" + minutes + (am ? "AM" : "PM");
+        return timeString;
+    }
+
+    state = {
+        tableHead: [],
+        tableData: [],
+        tableIDs: [],
+        dateSort: null,
+        locationSort: null
+    };
+
+    openSuccess() { console.log("Database is open!"); }
+    openError(err) { console.log("Error: ", err); }
+
+    closeDatabase = () => {
+        if (db) {
+            console.log("Closing database.");
+            db.close();
+        } else {
+            console.log("Database was not OPEN?!");
+        }
+    }
+
+    async componentDidMount() {
+        this.addButtons();
+        db = openDatabase({ name: 'KIWI.db', createFromLocation: 1 }, this.openSuccess, this.openError);
+        this.populateTable();
+    }
+
+    componentWillUnmount() {
+        this.closeDatabase();
+    }
+
+    retrieveUserID = async () => {
+        try {
+            user_ID = await AsyncStorage.getItem('user_ID');
+        } catch (error) {
+            alert(error);
+        }
+    }
+
+    dateSort = () => {
+        db.transaction(tx => {
+
+            let sql = "";
+
+            if (this.state.dateSort == null || this.state.dateSort) {
+                sql = "SELECT a_ID, a_eventID, a_date, a_time, a_locationname, a_eventname FROM archive WHERE a_ID = " + user_ID + " ORDER BY (a_date) ASC;";
+                this.setState({ dateSort: false });
+            }
+            else {
+                sql = "SELECT a_ID, a_eventID, a_date, a_time, a_locationname, a_eventname FROM archive WHERE a_ID = " + user_ID + " ORDER BY (a_date) DESC;";
+                this.setState({ dateSort: true });
+            }
+
+            tx.executeSql(sql, [], (tx, results) => {
+                const len = results.rows.length;
+                if (!len) {
+                } else {
+
+                    var idArr = [];
+                    var dataArr = [];
+
+                    for (let index = 0; index < len; index++) {
+
+                        var tempArr1 = [];
+                        var tempArr2 = [];
+
+                        const user = results.rows.item(index);
+
+                        tempArr1.push(user.a_ID);
+                        tempArr1.push(user.a_eventID);
+
+                        tempArr2.push(user.a_date);
+
+                        var array = user.a_time.split('.');
+                        array.pop();
+                        var string = array.join("");
+
+                        tempArr2.push(this.militaryToNormalTime(string));
+                        tempArr2.push(user.a_locationname);
+                        tempArr2.push(user.a_eventname);
+                        tempArr2.push("Delete");
+
+                        idArr.push(tempArr1);
+
+                        dataArr.push(tempArr2);
+
+                    }
+
+                    this.setState({ tableIDs: idArr });
+                    this.setState({ tableData: dataArr });
+
+                }
+            });
+        }, (err) => {
+            console.log('transaction error: ', err.message);
+        });
+    }
+
+    locationSort = () => {
+        db.transaction(tx => {
+
+            let sql = "";
+
+            if (this.state.locationSort == null || this.state.locationSort) {
+                sql = "SELECT a_ID, a_eventID, a_date, a_time, a_locationname, a_eventname FROM archive WHERE a_ID = " + user_ID + " ORDER BY (a_locationname) ASC;";
+                this.setState({ locationSort: false });
+            }
+            else {
+                sql = "SELECT a_ID, a_eventID, a_date, a_time, a_locationname, a_eventname FROM archive WHERE a_ID = " + user_ID + " ORDER BY (a_locationname) DESC;";
+                this.setState({ locationSort: true });
+            }
+
+            tx.executeSql(sql, [], (tx, results) => {
+                const len = results.rows.length;
+                if (!len) {
+                } else {
+
+                    var idArr = [];
+                    var dataArr = [];
+
+                    for (let index = 0; index < len; index++) {
+
+                        var tempArr1 = [];
+                        var tempArr2 = [];
+
+                        const user = results.rows.item(index);
+
+                        tempArr1.push(user.a_ID);
+                        tempArr1.push(user.a_eventID);
+
+                        tempArr2.push(user.a_date);
+
+                        var array = user.a_time.split('.');
+                        array.pop();
+                        var string = array.join("");
+
+                        tempArr2.push(this.militaryToNormalTime(string));
+                        tempArr2.push(user.a_locationname);
+                        tempArr2.push(user.a_eventname);
+                        tempArr2.push("Delete");
+
+                        idArr.push(tempArr1);
+
+                        dataArr.push(tempArr2);
+
+                    }
+
+                    this.setState({ tableIDs: idArr });
+                    this.setState({ tableData: dataArr });
+
+                }
+            });
+        }, (err) => {
+            console.log('transaction error: ', err.message);
+        });
+    }
+
+    addButtons = () => {
+
+        var Output = [];
+
+        const date = () => (
+            <Button onPress={() => this.dateSort()} style={{ backgroundColor: "blue", justifyContent: "center" }}><Text style={{ color: "white", fontSize: 12 }}>Date</Text></Button>
+        );
+
+        const loc_name = () => (
+            <Button onPress={() => this.locationSort()} style={{ backgroundColor: "blue", justifyContent: "center" }}><Text style={{ color: "white", fontSize: 12 }}>Location</Text></Button>
+        );
+
+        Output.push(date());
+        Output.push("Time");
+        Output.push(loc_name());
+        Output.push("Name");
+        Output.push("Delete?");
+
+        this.setState({ tableHead: Output });
+
+    }
+
+    populateTable = () => {
+        db.transaction(tx => {
+            let sql = "SELECT a_ID, a_eventID, a_date, a_time, a_locationname, a_eventname FROM archive WHERE a_ID = " + user_ID + ";";
+            tx.executeSql(sql, [], (tx, results) => {
+                const len = results.rows.length;
+                if (!len) {
+                } else {
+
+                    var idArr = [];
+                    var dataArr = [];
+
+                    for (let index = 0; index < len; index++) {
+
+                        var tempArr1 = [];
+                        var tempArr2 = [];
+
+                        const user = results.rows.item(index);
+
+                        tempArr1.push(user.a_ID);
+                        tempArr1.push(user.a_eventID);
+
+                        tempArr2.push(user.a_date);
+
+                        var array = user.a_time.split('.');
+                        array.pop();
+                        var string = array.join("");
+
+                        tempArr2.push(this.militaryToNormalTime(string));
+                        tempArr2.push(user.a_locationname);
+                        tempArr2.push(user.a_eventname);
+                        tempArr2.push("Delete");
+
+                        idArr.push(tempArr1);
+
+                        dataArr.push(tempArr2);
+
+                    }
+
+                    this.setState({ tableIDs: idArr });
+                    this.setState({ tableData: dataArr });
+
+                }
+            });
+        }, (err) => {
+            console.log('transaction error: ', err.message);
+        });
+    }
+
+    deleteEvent = (index) => {
+
+        var IDs = this.state.tableIDs;
+
+        var eventID = IDs[index][1];
+
+        db.transaction(tx => {
+            let sql = "DELETE FROM archive WHERE a_ID = " + user_ID + " AND a_eventID = " + eventID + ";";
+            tx.executeSql(sql, [], (tx, results) => {
+                alert('Deleted successfuly!');
+            });
+            this.populateTable();
+        }, (err) => {
+            console.log('transaction error: ', err.message);
+        });
+
+    }
+
     render() {
+        this.retrieveUserID();
+
+        const element = (index) => (
+            <Icon style={{ alignSelf: "center" }} name="trash" color={"blue"} size={20} onPress={() => this.deleteEvent(index)} />
+        );
+
         return (
             <View style={{ flex: 1, backgroundColor: "#BEED90" }}>
-                <Text style={{ fontSize: 22, color: "black", paddingLeft: 10, paddingRight: 10 }}>Also here, testing 1 2 3.</Text>
+                <Table borderStyle={{ borderColor: 'transparent' }}>
+                    <Row data={this.state.tableHead} style={styles.head} textStyle={styles.text} />
+                    {
+                        this.state.tableData.map((rowData, index) => (
+                            <TableWrapper key={index} style={styles.row}>
+                                {
+                                    rowData.map((cellData, cellIndex) => (
+                                        <Cell key={cellIndex} data={cellIndex === 4 ? element(index) : cellData} textStyle={styles.text} />
+                                    ))
+                                }
+                            </TableWrapper>
+                        ))
+                    }
+                </Table>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    head: { height: 40, backgroundColor: '#D3D3D3' },
+    text: { margin: 6, fontSize: 12 },
+    row: { flexDirection: 'row', backgroundColor: 'white' }
+});
--- statements.sql
+++ statements.sql
@@ -1,3 +1,64 @@
+/* LOGIN.JS */
+
+/* Retrieves data from login input (comparison done inside the application)*/
+SELECT u_ID, u_type FROM user WHERE u_name = 'username' AND u_password = 'password';
+
+/* FORGOT.JS */
+
+/* Retrieves user with information that matches user input */
+SELECT * FROM user WHERE u_ID = 'ID_Number' AND u_password = 'password';
+
+/* Retrieves user with information that matches user input */
+SELECT * FROM user WHERE u_ID = 'ID_Number' AND u_name = 'username';
+
+/* INFOCHANGE.JS */
+
+/* Checks to see if username does not already exist based on information given */
+SELECT * FROM user WHERE u_ID = 'ID_Number' AND u_name = 'username';
+
+/* Changes user name based on given information */
+UPDATE user SET u_name = 'username' WHERE u_ID = 'ID_Num';
+
+/* Changes password based on given information */
+UPDATE user SET u_password = 'password' WHERE u_ID = 'ID_Num';
+
+/* HOME.JS */
+
+/* INFORMATION.JS */
+
+/* ARCHIVE.JS */
+
+/* Displays archive of events based on user* (u_ID for id retrived) */
+SELECT a_ID, a_eventID, a_date, a_time, a_locationname, a_eventname FROM archive
+WHERE a_ID = 100123456;
+
+/* Deletes an event from the archive of the respective user (values will be grabbed from selected event) */
+DELETE FROM archive
+WHERE a_ID = 100123456 AND a_eventID = 10045454;
+
+/* Displays archive of events based on user and based on ascending date*/
+SELECT a_ID, a_eventID, a_date, a_time, a_locationname, a_eventname FROM archive
+WHERE a_ID = 100123456
+ORDER BY (a_date) ASC;
+
+/* Displays archive of events based on user and based on descending date*/
+SELECT a_ID, a_eventID, a_date, a_time, a_locationname, a_eventname FROM archive
+WHERE a_ID = 100123456
+ORDER BY (a_date) DESC;
+
+/* Displays archive of events based on user and based on ascending location name*/
+SELECT a_ID, a_eventID, a_date, a_time, a_locationname, a_eventname FROM archive
+WHERE a_ID = 100123456
+ORDER BY (a_locationname) ASC;
+
+/* Displays archive of events based on user and based on descending location name*/
+SELECT a_ID, a_eventID, a_date, a_time, a_locationname, a_eventname FROM archive
+WHERE a_ID = 100123456
+ORDER BY (a_locationname) DESC;
+
+
+
+
 /* Display/Store student information */
 SELECT ui_ID, ui_photoID, ui_qrID, ui_first_name, ui_last_name FROM user_info
 WHERE ui_user_type = 'student';
@@ -6,24 +67,10 @@ WHERE ui_user_type = 'student';
 SELECT ui_ID, ui_photoID, ui_qrID, ui_first_name, ui_last_name FROM user_info
 WHERE ui_user_type = 'professor';
 
-/* Retrieves data from login input (comparison done inside the application)*/
-SELECT u_ID, u_name, u_password, u_type FROM user;
-
-/* Displays archive of events based on user*/
-/* u_ID for id retrived */
-SELECT a_date, a_time, a_locationname, a_eventname FROM archive, events
-WHERE a_ID = 100123456 AND a_eventID = e_ID;
-
-/* Deletes an event from the archive of the respective user */
-/* values will be grabbed from selected event */
-DELETE FROM archive
-WHERE a_ID = 100123456 AND a_eventID = 10045454;
-
 /* Inserts a new student for the professor's roster */
 INSERT INTO roster VALUES (100111222, 'Test', 'Name', 0);
 
-/* Deletes a student from the professor's roster */
-/* values will be grabbed from selected student */
+/* Deletes a student from the professor's roster (values will be grabbed from selected student) */
 DELETE FROM roster
 WHERE r_userID = 100111222 AND r_firstname = 'Test' AND r_lastname = 'Name';
 
@@ -35,22 +82,9 @@ WHERE u_ID = 100123456;
 /* Displays the full list of classes */
 SELECT c_time, c_roomnum, c_professor, c_classname FROM classes;
 
-/* Inserts a new event for the user's archive */
-INSERT INTO archive VALUES (100123456, 10000333, '2019-10-01', '10:00:00.000', 'COB 102', 'MATH 005-LEC');
-
 /* Displays professor roster */
 SELECT r_userID, r_firstname, r_lastname, r_last_check_in FROM roster
 WHERE r_userID = 100789123;
-
-/* Update password */
-UPDATE user
-SET u_password = 'Blahblah12@'
-WHERE u_ID = '100123456';
-
-/* Update username */
-UPDATE user
-SET u_username = 'testusername'
-WHERE u_ID = '100123456' AND u_password = 'Blahblah12@';
 
 /* Displays professor roster based on check-in (true) */
 SELECT r_userID, r_firstname, r_lastname, r_last_check_in FROM roster, user_info
@@ -79,23 +113,3 @@ ORDER BY (r_lastname) ASC;
 SELECT r_userID, r_firstname, r_lastname, r_last_check_in FROM roster, user_info
 WHERE r_userID = ui_ID
 ORDER BY (r_lastname) DESC;
-
-/* Displays archive of events based on user and based on ascending date*/
-SELECT a_date, a_time, a_locationname, a_eventname FROM archive, events, user_info
-WHERE a_ID = ui_ID AND a_eventID = e_ID
-ORDER BY (a_date) ASC;
-
-/* Displays archive of events based on user and based on descending date*/
-SELECT a_date, a_time, a_locationname, a_eventname FROM archive, events, user_info
-WHERE a_ID = ui_ID AND a_eventID = e_ID
-ORDER BY (a_date) DESC;
-
-/* Displays archive of events based on user and based on ascending location name*/
-SELECT a_date, a_time, a_locationname, a_eventname FROM archive, events, user_info
-WHERE a_ID = ui_ID AND a_eventID = e_ID
-ORDER BY (a_locationname) ASC;
-
-/* Displays archive of events based on user and based on descending location name*/
-SELECT a_date, a_time, a_locationname, a_eventname FROM archive, events, user_info
-WHERE a_ID = ui_ID AND a_eventID = e_ID
-ORDER BY (a_locationname) DESC;
